0b3106ee95db0f04aee8fb106fea8e9b9d5b0bd2
Thibault <Thibault@mail.com>
2019-12-05 22:38:30 +0000

    Chat de base fonctionnel

--- server.js
+++ server.js
@@ -3,27 +3,24 @@ const app     = express();
 const server  = require('http').createServer(app);
 const io      = require('socket.io')(server);
 
-
 app.use(express.static('public'));
 app.use('/resources', require('express').static(__dirname + '/node_modules/'));
 
 io.on('connection', function(client) {
+
 	console.log('New user is connected');
 
 	client.on('evt1', function(data) {
 		console.log('Chat event');
 		console.log(data);
 		console.log(data.message);
-		io.emit('printLog', "Message reçu");
+		client.emit('printLog', "Message reçu");
+		io.emit('majChat', data);
 	});
+
 });
+
 server.listen(8080, function() {
 	console.log('Server running on port 8080');
 });
 
-
-/*
-app.listen(8080, function() {
-	console.log('Server up and running on port 8080');
-});
-*/
