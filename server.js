d611456e2dba5c50cbe998bb94f2bd0cf9517c15
ThibaultAjas <Thibault@mail.com>
2019-12-06 05:06:22 +0100

    tests, si ca fonctionne pas je me casse

--- server.js
+++ server.js
@@ -20,30 +20,57 @@ app.use(session({
 }));
 
 app.use(function(req, res) {
-  res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
+  // res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
   console.log(req.sessionID);
 });
 
+var Session = require('connect').middleware.session.Session;
+
 io.set('authorization', function(data, accept) {
-  // check if there's a cookie header
   if (data.headers.cookie) {
-    // if there is, parse the cookie
-    data.cookie = cookieParser(data.headers.cookie);
-    // note that you will need to use the same key to grad the
-    // session id, as you specified in the Express setup.
+    data.cookie = parseCookie(data.headers.cookie);
     data.sessionID = data.cookie['express.sid'];
+    // save the session store to the data object
+    // (as required by the Session constructor)
+    data.sessionStore = sessionStore;
+    sessionStore.get(data.sessionID, function(err, session) {
+      if (err || !session) {
+        accept('Error', false);
+      } else {
+        // create a session object, passing data as request and our
+        // just acquired session data
+        data.session = new Session(data, session);
+        accept(null, true);
+      }
+    });
   } else {
-    // if there isn't, turn down the connection with a message
-    // and leave the function.
     return accept('No cookie transmitted.', false);
   }
-  // accept the incoming connection
-  accept(null, true);
 });
 
 io.on('connection', function(client) {
-  console.log('New user is connected');
-  console.log('A socket with sessionID ' + client.handshake.sessionID + ' connected!');
+  var hs = client.handshake;
+  console.log('A socket with sessionID ' + hs.sessionID +
+    ' connected!');
+  // setup an inteval that will keep our session fresh
+  var intervalID = setInterval(function() {
+    // reload the session (just in case something changed,
+    // we don't want to override anything, but the age)
+    // reloading will also ensure we keep an up2date copy
+    // of the session with our connection.
+    hs.session.reload(function() {
+      // "touch" it (resetting maxAge and lastAccess)
+      // and save it back again.
+      hs.session.touch().save();
+    });
+  }, 60 * 1000);
+  client.on('disconnect', function() {
+    console.log('A socket with sessionID ' + hs.sessionID +
+      ' disconnected!');
+    // clear the socket interval to stop refreshing the session
+    clearInterval(intervalID);
+  });
+
 });
 
 
