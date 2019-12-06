aac2c354f90e04e1d6078e6bb864434cf36a0b3b
ThibaultAjas <Thibault@mail.com>
2019-12-06 04:59:14 +0100

    fix parsecookie

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.set('authorization', function(data, accept) {
   // check if there's a cookie header
   if (data.headers.cookie) {
     // if there is, parse the cookie
-    data.cookie = parseCookie(data.headers.cookie);
+    data.cookie = cookieParser(data.headers.cookie);
     // note that you will need to use the same key to grad the
     // session id, as you specified in the Express setup.
     data.sessionID = data.cookie['express.sid'];
